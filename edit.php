32f857f519fa723434c961eb15c9e5e27a81b0c1
JULES RACHET <rachet.jules@gmail.com>
2020-10-08 14:20:29 +0200

    10 min left

---
+++ edit.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<body>
+<style>
+div.mg1 {
+  margin-left: 35em;
+}
+</style>
+<div class="mg1"><a href="index.php"><button type="button">Back to List</button></a></div>
+<h2>Editer un article </h2>
+
+<form action="" method ="POST">
+  <label for="fname">Title</label><br>
+  <input type="text" id="title" name="title" value=""><br>
+  <label for="lname">Description</label><br>
+  <input type="text" id="description" name="description" ><br><br>
+  <label for="lname">Date</label><br>
+  <input type="date" id="date" name="date" ><br><br>
+  <input type="submit" value="Add">
+</form> 
+
+<p>Click the Add button to submit it the database.</p>
+
+</body>
+</html>
+
+<?php 
+print_r($_GET);
+if (!empty($_POST)) {
+  // Only process if form is submitted
+  require 'db.php';
+
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  
+    $conn = new PDO("mysql:host=$servername;dbname=phpsamples", $username, $password);
+
+
+
+  $title = $_POST['title'];
+  $description = $_POST['description'];
+  $date = $_POST['date'];
+
+    if (!empty($date))
+    {
+        $stmt = $conn->prepare("INSERT INTO posts (post_title, description, post_at) VALUES (:title, :description, :date)");
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':date', $date);
+    }
+    else
+    {
+        $stmt = $conn->prepare("INSERT INTO posts (post_title, description) VALUES (:title, :description)");
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':description', $description);
+    }
+   
+
+  $stmt->execute();
+  exit;
+
+}
+
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,56 @@
+
 <?php
 
+function printtable($sql)
+{
+$servername = "localhost";
+$username = "root";
+$password = "";
+$count = 1;
+$conn = new PDO("mysql:host=$servername;dbname=phpsamples", $username, $password);
+
+
+    $sql = "select * from posts";
+    $res = $conn -> query($sql);
+    $tab = $res -> fetchall(PDO::FETCH_ASSOC);
+    // print_r($tab);
+
+        foreach ($tab as $value => $field)
+        {
+        $id = $field['id'];
+        $title ="<td>".$field['post_title']."</td>";
+        $description = "<td>".$field['description']."</td>";
+        $postdate = "<td>".$field['post_at']."</td>";
+        $action = "<td><a href= 'edit.php?{$id}'> <img src='crud-icon/edit.png'></a>
+        <a href='delete.php?{$id}'> <img src='crud-icon/delete.png'></a>
+        </td>";
+        $row ="<tr>".$title.$description.$postdate.$action."</tr>";
+        echo $row; 
+        }
+}
+
 ?>
+<!DOCTYPE html>
+<html>
+<body>
+    <style>
+div.mg1 {
+  margin-left: 70em;
+}
+</style>
+<div class="mg1"><a href="add.php"><button type="button">Create</button></a></div>
+<h2>Basic HTML Table</h2>
+
+<table border style="width:100%">
+  <tr>
+    <th>Title</th>
+    <th>Description</th> 
+    <th>Date</th>
+    <th>Actions</th>
+  </tr>
+<?= printtable("select * from posts")?>
+       
+</table>
+
+</body>
+</html>
